8626ad2fdf475039253bcda1f4ff92d0d6a4e531
hons82 <hons82@gmail.com>
2014-07-25 14:12:22 +0200

    Initial Commit

---
+++ THSegmentedPagerExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  THSegmentedPagerExample
+//
+//  Created by Hannes Tribus on 25/07/14.
+//  Copyright (c) 2014 3Bus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ THSegmentedPagerExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  THSegmentedPagerExample
+//
+//  Created by Hannes Tribus on 25/07/14.
+//  Copyright (c) 2014 3Bus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
