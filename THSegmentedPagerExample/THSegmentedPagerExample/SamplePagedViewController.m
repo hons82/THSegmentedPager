1487902cafccc533eea37375279d519318bc72c3
Victor Ilyukevich <ilyukevich.victor@gmail.com>
2015-04-18 11:07:51 -0700

    Cleanup: remove code doing nothing

--- THSegmentedPagerExample/THSegmentedPagerExample/SamplePagedViewController.m
+++ THSegmentedPagerExample/THSegmentedPagerExample/SamplePagedViewController.m
@@ -14,29 +14,6 @@
 
 @implementation SamplePagedViewController
 
-@synthesize viewTitle = _viewTitle;
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 - (NSString *)viewControllerTitle
 {
     return self.viewTitle ? self.viewTitle : self.title;
